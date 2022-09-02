35e1314fcde44e7b57be7890066a65e006d892d5
Axel Carlos Lopez <lopezaxel@protonmail.com>
2022-09-02 14:40:29 -0300

    Update router

--- src/Routes/RouteSwitch.js
+++ src/Routes/RouteSwitch.js
@@ -1,5 +1,14 @@
 import { Route, Routes, ReactRouter } from "react-router-dom";
 
 export default function RouteSwitch() {
-  return <ReactRouter></ReactRouter>;
+  return (
+    <ReactRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="about" element={<About />} />
+          <Route path="projects" element={<Projects />} />
+        </Route>
+      </Routes>
+    </ReactRouter>
+  );
 }
